cd7448fce4eb6f0f01b0d6427dcf9ce1e2b7123e
iulik98 <fintinaiulian6@gmail.com>
2023-06-20 09:05:42 +0300

    Implemented PageFactory design pattern for newly added classes and refactored the TestRunner class.

---
+++ src/main/java/com/opencart/pageobjects/LoginPage.java
@@ -0,0 +1,36 @@
+package com.opencart.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends Page{
+
+    @FindBy(css = "#input-email")
+    private WebElement emailAddress;
+
+    @FindBy(css = "#input-password")
+    private WebElement passwordLogIn;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement logInButton;
+
+    @FindBy(linkText = "Forgotten Password")
+    private WebElement forgotPassLink;
+    public LoginPage(WebDriver driver){
+        super(driver);
+    }
+
+    public void fillTheLoginForm(String email, String pass){
+        emailAddress.sendKeys(email);
+        passwordLogIn.sendKeys(pass);
+    }
+
+    public void clickOnLogInButton(){
+        logInButton.click();
+        System.out.println("The login button was clicked.");
+    }
+
+
+
+}
