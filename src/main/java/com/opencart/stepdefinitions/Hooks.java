d5299a689bd5f366d8a966ccd45b7dc4f4751123
iulik98 <fintinaiulian6@gmail.com>
2023-06-26 20:34:08 +0300

    Added Cucumber Implementation and added first two scenarios

---
+++ src/main/java/com/opencart/stepdefinitions/Hooks.java
@@ -0,0 +1,34 @@
+package com.opencart.stepdefinitions;
+
+import com.opencart.managers.DriverManager;
+import com.opencart.pageobjects.HomePage;
+import com.opencart.pageobjects.RegisterPage;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+public class Hooks {
+    static int counter = 0;
+    @BeforeAll
+    public static void beforeAllTheTestsAreExecuted(){
+        System.out.println("The execution starts");
+    }
+    @AfterAll
+    public static void afterAllTheTestsAreExecuted(){
+        System.out.println("The execution of all tests is terminated.");
+    }
+
+    @Before
+    public void beforeEachTest(){
+        counter++;
+        System.out.println("The test number: " + counter + " started");
+    }
+
+    @After
+    public void afterEachTest(){
+        DriverManager.getInstance().quitWebDriver();
+        System.out.println("The test with number:" + counter + " execution finished");
+    }
+}
